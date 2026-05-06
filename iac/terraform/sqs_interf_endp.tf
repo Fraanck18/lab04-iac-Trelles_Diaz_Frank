4dcf941a130f58b75a01d4aaf77158127e362f95
Fraanck18 <andersonjaird05@gmail.com>
2026-05-06 18:13:19 -0500

    fix: solucionando errores de sqs interfaz

--- iac/terraform/sqs_interf_endp.tf
+++ iac/terraform/sqs_interf_endp.tf
@@ -1,38 +1,31 @@
 # CONFIGURACION SQS_Interf_Endpoint 
 resource "aws_vpc_endpoint" "sqs_interface" {
-  vpc_id            = aws_vpc.main.id
-  
-  # CONFIGURACION \nType 
-  vpc_endpoint_type = "Interface"
-
-  # CONFIGURACION \nService
-  service_name      = "com.amazonaws.us-east-1.sqs"
-
-  # CONFIGURACION \nPrivate DNS
+  vpc_id              = aws_vpc.main.id
+  vpc_endpoint_type   = "Interface"
+  service_name        = "com.amazonaws.${var.region}.sqs"
   private_dns_enabled = true
 
-  # CONFIGURACION \nDeployed 
   subnet_ids = [
     aws_subnet.private_az_a.id,
     aws_subnet.private_az_b.id
   ]
 
-  # CONFIGURACION \nSG
   security_group_ids = [aws_security_group.sg_vpce_sqs.id]
 
   tags = {
-    Name    = "sqs-interface-endpoint"
-    Project = "PROC-IMG-API"
+    Name    = "sqs-interface-endpoint-${terraform.workspace}"
+    Project = var.project_name
   }
 }
 
+
+
 # SECURITY GROUP 
 resource "aws_security_group" "sg_vpce_sqs" {
-  name        = "sg-vpce-sqs"
+  name        = "vpce-sqs-sg-${terraform.workspace}"
   description = "Security group for SQS Interface Endpoint"
   vpc_id      = aws_vpc.main.id
 
-  # CONFIGURACION \nInbound TCP
   ingress {
     from_port       = 443
     to_port         = 443
@@ -40,7 +33,6 @@ resource "aws_security_group" "sg_vpce_sqs" {
     security_groups = [aws_security_group.sg_upload_lambda.id]
   }
 
-  # CONFIGURACION \nInbound TCP
   ingress {
     from_port       = 443
     to_port         = 443
@@ -48,7 +40,14 @@ resource "aws_security_group" "sg_vpce_sqs" {
     security_groups = [aws_security_group.sg_crop_lambda.id]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
-    name = "vpce-sqs-sg-${terraform.workspace}"
+    Name = "vpce-sqs-sg-${terraform.workspace}"
   }
 }
