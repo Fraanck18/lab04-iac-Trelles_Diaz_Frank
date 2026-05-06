1e4ba664c84aca12cc4dc7d9324658e7ac9e098f
Fraanck18 <andersonjaird05@gmail.com>
2026-05-06 18:04:34 -0500

    feature: Actualizando parametros y variables

--- iac/terraform/variables.tf
+++ iac/terraform/variables.tf
@@ -1,9 +1,17 @@
-variable "aws_region" {
-    description = "Region de AWS de despliegue"
-    type        = string
-    default     = "us-east-1"
+# CONFIGURACION REGION Y PROYECTO
+variable "region" {
+  description = "Region de AWS de despliegue"
+  type        = string
+  default     = "us-east-1"
 }
 
+variable "project_name" {
+  description = "Nombre base para los recursos"
+  type        = string
+  default     = "PROC-IMG-API"
+}
+
+#CONFIGURACION DE ENTORNOS 
 variable "aws_profiles" {
   type = map(string)
   default = {
@@ -13,13 +21,43 @@ variable "aws_profiles" {
   }
 }
 
-# DEFINIMOS PARAMETROS
 locals {
   aws_profile = var.aws_profiles[terraform.workspace]
 }
 
-#PARAMETROS PARA VPC
+#PARAMETROS DE RED 
 variable "vpc_cidr" {
   description = "Rango de IPs para la VPC"
   type        = string
+}
+
+variable "public_subnets" {
+  description = "Lista de CIDRs para subredes publicas"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  description = "Lista de CIDRs para subredes privadas"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+#PARAMETROS DE APLICACION 
+variable "retention_in_days" {
+  description = "Retencion de logs en CloudWatch"
+  type        = number
+  default     = 14
+}
+
+variable "upload_prefix" {
+  description = "Carpeta de origen en S3"
+  type        = string
+  default     = "uploads/"
+}
+
+variable "processed_prefix" {
+  description = "Carpeta de destino en S3"
+  type        = string
+  default     = "processed/"
 }
